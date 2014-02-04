6e2c764bad1ecd73619cf4dc7bebe8e8b66ec38c
jekil <alessandro@tanasi.it>
2014-02-04 07:43:38 +0100

    Fixed quotes

--- analyses/templatetags/analyses_tags.py
+++ analyses/templatetags/analyses_tags.py
@@ -19,8 +19,8 @@ def mongo_id(value):
     """
     # Retrieve _id value
     if type(value) == type({}):
-        if value.has_key('_id'):
-            value = value['_id']
+        if value.has_key("_id"):
+            value = value["_id"]
    
     # Return value
     return unicode(value)
