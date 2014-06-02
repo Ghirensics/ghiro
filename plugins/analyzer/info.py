3397df30de0434de5dbd307804b0733bf0fdba9d
jekil <alessandro@tanasi.it>
2014-06-02 11:52:39 +0200

    Added basic information module

---
+++ plugins/analyzer/info.py
@@ -0,0 +1,22 @@
+# Ghiro - Copyright (C) 2013-2014 Ghiro Developers.
+# This file is part of Ghiro.
+# See the file 'docs/LICENSE.txt' for license terms.
+
+from lib.analyzer.base import BaseAnalyzerModule
+
+
+class InfoAnalyzer(BaseAnalyzerModule):
+    """Collect basic information."""
+
+    order = 10
+
+    def check_deps(self):
+        return True
+
+    def run(self, task):
+        # File name.
+        self.results["file_name"] = task.file_name
+        # File size
+        self.results["file_size"] = task.get_file_length
+
+        return self.results
