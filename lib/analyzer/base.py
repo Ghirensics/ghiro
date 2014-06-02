8613d9c8b19f46527ecb336803c691eda3598ef2
jekil <alessandro@tanasi.it>
2014-06-02 11:40:20 +0200

    Added module order and base signature

---
+++ lib/analyzer/base.py
@@ -0,0 +1,32 @@
+# Ghiro - Copyright (C) 2013-2014 Ghiro Developers.
+# This file is part of Ghiro.
+# See the file 'docs/LICENSE.txt' for license terms.
+
+from lib.utils import AutoVivification
+
+class BaseAnalyzerModule(object):
+
+    # Module execution order.
+    order = 0
+
+    def __init__(self):
+        # Results storage.
+        self.results = AutoVivification()
+        # Data (results) in the global dictionary (produced by other modules).
+        self.data = None
+
+    def check_deps(self):
+        raise NotImplementedError
+
+    def run(self, task):
+        raise NotImplementedError
+
+class BaseSignature(object):
+    pk = 0
+    severity = 0
+    category = None
+    name = None
+    description = None
+
+    def check(self, data):
+        raise NotImplementedError
