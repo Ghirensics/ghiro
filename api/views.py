07e693d8f6374af4a12b1a9e9bab247461019bdf
jekil <alessandro@tanasi.it>
2014-06-17 23:43:30 +0200

    Added API for submitting images

--- api/views.py
+++ api/views.py
@@ -3,16 +3,17 @@
 # See the file 'docs/LICENSE.txt' for license terms.
 
 import json
-import analyses.forms as forms
 
-from django.shortcuts import render
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
-from ghiro.common import log_activity, mongo_connect, check_allowed_content
+from ghiro.common import log_activity
 from ghiro.authorization import api_authenticate
-from analyses.models import Case
+from analyses.models import Case, Analysis
+from lib.db import save_file
+from lib.utils import create_thumb
 
 @require_POST
 @csrf_exempt
@@ -36,3 +37,36 @@ def new_case(request):
         return HttpResponse(json.dumps(response_data), content_type="application/json")
     else:
         return HttpResponse("Request not valid", status=400)
+
+@require_POST
+@csrf_exempt
+def new_image(request):
+    """Upload a new image."""
+    user = api_authenticate(request.POST.get("api_key"))
+
+    case = get_object_or_404(Case, pk=request.POST.get("case_id"))
+
+    # Security check.
+    if not user.is_superuser and not user in case.users.all():
+        return HttpResponse("You are not authorized to add image to this", status=400)
+
+    if case.state == "C":
+        return HttpResponse("You cannot add an image to a closed case", status=400)
+
+    task = Analysis(owner=user,
+                    case=case,
+                    file_name=request.FILES["image"].name,
+                    image_id=save_file(file_path=request.FILES["image"].temporary_file_path(),
+                              content_type=request.FILES["image"].content_type),
+                    thumb_id=create_thumb(request.FILES["image"].temporary_file_path())
+    )
+    task.save()
+
+    # Auditing.
+    log_activity("I",
+                 "Created new analysis {0}".format(task.file_name),
+                 request,
+                 user=user)
+
+    response_data = {"id": task.id}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
