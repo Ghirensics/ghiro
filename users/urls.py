ccf140fe290a3a8965b43e6b3e66d1b69bb371e1
jekil <alessandro@tanasi.it>
2014-10-10 00:14:03 +0200

    Fixed bug in password change urls

--- users/urls.py
+++ users/urls.py
@@ -2,14 +2,15 @@
 # This file is part of Ghiro.
 # See the file 'docs/LICENSE.txt' for license terms.
 
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns("",
     (r"^login/$", "django.contrib.auth.views.login", {"template_name": "users/login.html"}),
     (r"^logout/$", "django.contrib.auth.views.logout", {"next_page": "/"}),
     (r"^profile/$", "users.views.profile"),
     (r"^password_change/$", "django.contrib.auth.views.password_change", {"template_name": "users/password_change.html"}),
-    (r"^password_change_done/$", "django.contrib.auth.views.password_change_done", {"template_name": "users/password_change_done.html"}),
+    (r"^password_change_done/$", "django.contrib.auth.views.password_change_done" ,{"template_name": "users/password_change_done.html"}, "password_change_done"),
 
     # Admin
     (r"^management/index/$", "users.views.admin_list_users"),
