3afcaf68a5a7d072addb976bcb37d71586487522
jekil <alessandro@tanasi.it>
2015-06-03 12:12:01 +0200

    Added custom exceptions

---
+++ lib/exceptions.py
@@ -0,0 +1,11 @@
+# Ghiro - Copyright (C) 2013-2015 Ghiro Developers.
+# This file is part of Ghiro.
+# See the file 'docs/LICENSE.txt' for license terms.
+
+class GhiroException(Exception):
+    """Base Ghiro exception."""
+    pass
+
+class GhiroValidationException(GhiroException):
+    """Validation error."""
+    pass
