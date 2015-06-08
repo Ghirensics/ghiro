427de0cde617f3fb6ee89ae6d3257360c05bdd66
jekil <alessandro@tanasi.it>
2015-06-08 23:20:38 +0200

    Moved tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,25 +9,6 @@ from django.test import TestCase
 from users.models import Profile
 from analyses.models import Case, Analysis
 
-class AuthenticationTest(TestCase):
-    def setUp(self):
-        self.user = Profile.objects.create_user(username="test", email="a@a.cp,", password="Test")
-        self.c = Client()
-
-    def test_fail_new_cas_no_api(self):
-        """Tests failed auth with no api key."""
-        response = self.c.post("/api/cases/new", {"name": "test", "description": "test"})
-        self.assertEqual(response.status_code, 403)
-
-    def test_fail_new_case_wrong_api(self):
-        """Tests failed auth with wrong api key."""
-        response = self.c.post("/api/cases/new", {"name": "test", "description": "test", "api_key": "aaaaa"})
-        self.assertEqual(response.status_code, 403)
-
-    def test_success_new_case(self):
-        """Tests success auth.."""
-        response = self.c.post("/api/cases/new", {"name": "test", "description": "test", "api_key": self.user.api_key})
-        self.assertEqual(response.status_code, 200)
 
 class NewCaseTest(TestCase):
     def setUp(self):
@@ -43,6 +24,21 @@ class NewCaseTest(TestCase):
         response = self.c.post("/api/cases/new", {"name": "test2", "api_key": self.user.api_key})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Case.objects.filter(name="test2").exists())
+
+    def test_fail_auth_new_case_no_api(self):
+        """Tests failed auth with no api key."""
+        response = self.c.post("/api/cases/new", {"name": "test", "description": "test"})
+        self.assertEqual(response.status_code, 403)
+
+    def test_fail_auth_new_case_wrong_api(self):
+        """Tests failed auth with wrong api key."""
+        response = self.c.post("/api/cases/new", {"name": "test", "description": "test", "api_key": "aaaaa"})
+        self.assertEqual(response.status_code, 403)
+
+    def test_success_auth_new_case(self):
+        """Tests success auth.."""
+        response = self.c.post("/api/cases/new", {"name": "test", "description": "test", "api_key": self.user.api_key})
+        self.assertEqual(response.status_code, 200)
 
 class NewImageTest(TestCase):
     def setUp(self):
