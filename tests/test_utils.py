24ce80244893fc94300e1c4f5e3305bd182d65a6
jekil <alessandro@tanasi.it>
2015-06-21 23:30:27 +0200

    Added more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@
 # This file is part of Ghiro.
 # See the file 'docs/LICENSE.txt' for license terms.
 
+import os
 from django.test import TestCase
 
 from lib.utils import *
@@ -14,3 +15,11 @@ class ImportIsAvailableTest(TestCase):
 
     def test_cannot_import(self):
         self.assertFalse(import_is_available("antanitapioco"))
+
+class GetContentTypeFromFileTest(TestCase):
+
+    def setUp(self):
+        self.image = os.path.join("tests", "fixtures", "images", "1x1.png")
+
+    def test_content_type(self):
+        self.assertEqual(get_content_type_from_file(self.image), "image/png")
