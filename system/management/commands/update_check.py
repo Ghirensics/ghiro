03b11506357dbaa36903653f9ab96b09a801558b
jekil <alessandro@tanasi.it>
2015-08-16 18:15:55 +0200

    Added option to force an update check

--- system/management/commands/update_check.py
+++ system/management/commands/update_check.py
@@ -2,6 +2,7 @@
 # This file is part of Ghiro.
 # See the file 'docs/LICENSE.txt' for license terms.
 
+from optparse import make_option
 from django.core.management.base import NoArgsCommand
 
 from ghiro.common import check_version
@@ -12,15 +13,24 @@ class Command(NoArgsCommand):
 
     help = "Checks for new Ghiro releases"
 
-    option_list = NoArgsCommand.option_list
+    option_list = NoArgsCommand.option_list + (
+        make_option("-f", "--force", action="store_true", dest="force"),
+    )
 
     def handle(self, *args, **options):
         """Runs command."""
 
         print("Starting update check...")
 
+        # If force is enabled we don't use cached response.
+        if options["force"]:
+            print("Enabled forced check.")
+            force = True
+        else:
+            force = False
+
         try:
-            new_release = check_version()
+            new_release = check_version(force=force)
         except Exception as e:
             print("Error occurred: %s" % e)
         else:
