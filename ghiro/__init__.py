4181e3f1de18dca41a8396689a9c26604c59c541
jekil <alessandro@tanasi.it>
2015-10-11 18:50:51 +0200

    Moved initailization code to a startup appconfig

--- ghiro/__init__.py
+++ ghiro/__init__.py
@@ -1,15 +1,3 @@
 # Ghiro - Copyright (C) 2013-2015 Ghiro Developers.
 # This file is part of Ghiro.
 # See the file 'docs/LICENSE.txt' for license terms.
-
-from pymongo import GEO2D
-
-# Mongo connection.
-from lib.db import mongo_connect
-
-db = mongo_connect()
-
-# Indexes.
-db.fs.files.create_index("sha1", unique=True, name="sha1_unique")
-db.fs.files.create_index("uuid", unique=True, name="uuid_unique")
-db.analyses.create_index([("metadata.gps.pos", GEO2D)])
---
+++ system/apps.py
@@ -0,0 +1,25 @@
+# Ghiro - Copyright (C) 2013-2015 Ghiro Developers.
+# This file is part of Ghiro.
+# See the file 'docs/LICENSE.txt' for license terms.
+
+from django.apps import AppConfig
+from pymongo import GEO2D
+
+# Mongo connection.
+from lib.db import mongo_connect
+
+
+class SystemConfig(AppConfig):
+    name = "system"
+    verbose_name = "System Application"
+
+    def ready(self):
+        """Initialization code.
+        It runs once at app startup.
+        """
+        db = mongo_connect()
+
+        # Indexes.
+        db.fs.files.create_index("sha1", unique=True, name="sha1_unique")
+        db.fs.files.create_index("uuid", unique=True, name="uuid_unique")
+        db.analyses.create_index([("metadata.gps.pos", GEO2D)])
