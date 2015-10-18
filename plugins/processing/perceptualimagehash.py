425e2350c930474a3523c2e4f8eb408c229a0584
jekil <alessandro@tanasi.it>
2015-10-19 00:07:58 +0200

    Added similarity search

--- plugins/processing/perceptualimagehash.py
+++ plugins/processing/perceptualimagehash.py
@@ -2,6 +2,7 @@
 # This file is part of Ghiro.
 # See the file 'docs/LICENSE.txt' for license terms.
 
+from analyses.models import Case
 from lib.analyzer.base import BaseProcessingModule
 
 from lib.utils import str2image
@@ -23,11 +24,40 @@ class PerceptualImageHashProcessing(BaseProcessingModule):
     def check_deps(self):
         return IS_IMAGEHASH
 
+    def get_similar_images(self, hash_value, hash_func):
+        # TODO: this should be refactored in the future.
+
+        # Map.
+        if hash_func == imagehash.average_hash:
+            hash_name = "a_hash"
+        elif hash_func == imagehash.phash:
+            hash_name = "p_hash"
+        elif hash_func == imagehash.dhash:
+            hash_name = "d_hash"
+
+        # Search.
+        image_hash = imagehash.hex_to_hash(hash_value)
+        similarities = set()
+        for img in self.task.case.images.all():
+            if img.state == "C" and img.report and \
+            "imghash" in img.report and \
+            hash_name in img.report["imghash"] and \
+            image_hash == imagehash.hex_to_hash(img.report["imghash"][hash_name]):
+                similarities.add(self.task.id)
+        return list(similarities)
+
     def run(self, task):
+        self.task = task
         image = str2image(task.get_file_data)
 
+        # Calculate hash.
         self.results["imghash"]["a_hash"] = str(imagehash.average_hash(image))
         self.results["imghash"]["p_hash"] = str(imagehash.phash(image))
         self.results["imghash"]["d_hash"] = str(imagehash.dhash(image))
+
+        # Get similar images.
+        self.results["similar"]["a_hash"] = self.get_similar_images(self.results["imghash"]["a_hash"], imagehash.average_hash)
+        self.results["similar"]["p_hash"] = self.get_similar_images(self.results["imghash"]["p_hash"], imagehash.phash)
+        self.results["similar"]["d_hash"] = self.get_similar_images(self.results["imghash"]["d_hash"], imagehash.dhash)
 
         return self.results
---
+++ plugins/vfs/mongo_gridfs.py
@@ -0,0 +1,79 @@
+# Ghiro - Copyright (C) 2013-2015 Ghiro Developers.
+# This file is part of Ghiro.
+# See the file 'docs/LICENSE.txt' for license terms.
+
+import sys
+import hashlib
+import uuid
+from django.conf import settings
+import gridfs
+from bson import ObjectId
+from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.errors import ConnectionFailure
+
+
+def mongo_connect():
+    """Connects to Mongo, exits if unable to connect.
+    @return: connection handler
+    """
+    try:
+        db = Database(MongoClient(settings.MONGO_URI), settings.MONGO_DB)
+    except ConnectionFailure:
+        print "ERROR: unable to connect to MongoDB. Please check the server availability."
+        sys.exit()
+    else:
+        return db
+
+# Mongo connection.
+db = mongo_connect()
+fs = gridfs.GridFS(db)
+
+def save_file(data=None, file_path=None, content_type=None):
+    """Save file in GridFS.
+    @param data: file data
+    @param file_path: file path
+    @param content_type: file content type
+    @return: saved file ID
+    """
+    if file_path:
+        try:
+            fh = open(file_path, "rb")
+            data = fh.read()
+        finally:
+            fh.close()
+
+    # Using SHA1 as file key.
+    sha1 = hashlib.sha1(data).hexdigest()
+
+    # File identifier.
+    id = uuid.uuid4().hex + sha1
+
+    # Save file and returns UUID.
+    try:
+        fs.put(data, content_type=content_type, sha1=sha1, uuid=id)
+    except gridfs.errors.FileExists:
+        id = db.fs.files.find_one({"sha1": sha1})["uuid"]
+    finally:
+        return id
+
+def get_file(id):
+    """Get a file from GridFS.
+    @param id: file uuid
+    @return: file object"""
+    obj_id = db.fs.files.find_one({"uuid": id})["_id"]
+    return fs.get(ObjectId(obj_id))
+
+def get_file_length(id):
+    """Get a file length from GridFS.
+    @param id: file uuid
+    @return: integer"""
+
+    return db.fs.files.find_one({"uuid": id})["length"]
+
+def save_results(results):
+    """Save results in mongo.
+    @param results: data dict
+    @return: object id
+    """
+    return db.analyses.save(results)
