5391059234fca5501ed171bd036f3a3f65810588
jekil <alessandro@tanasi.it>
2016-05-15 14:31:21 +0200

    Set Static root

--- ghiro/settings.py
+++ ghiro/settings.py
@@ -41,7 +41,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
